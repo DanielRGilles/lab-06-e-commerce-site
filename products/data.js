a9d27c994cd438abc38b69b52b969eb78bde7c84
DanielRGilles <danryangilles@gmail.com>
2021-08-23 14:08:13 -0700

    added data and object literals

--- products/data.js
+++ products/data.js
@@ -1,11 +1,54 @@
-const beans = [
+export const beans = [
     {
         id: 0,
-        name: 'scarlet runner bean',
-        image: '../assets/'
+        name: 'Scarlet runner bean',
+        image: 'scarletr.jfif',
+        description: 'a large pole variety',
+        price:  3,
+        germination: '7 to 14'
+
+
+    },
+    {
+        id: 1,
+        name: 'Appaloosa',
+        image: 'appalb.jfif',
+        description: 'a medium hybrid variety',
+        price:  3,
+        germination: '8 to 10'
+
+
+    },
+    {
+        id: 2,
+        name: 'Vaquero bean',
+        image: 'vaquero.jfif',
+        description: 'a short bush variety',
+        price:  3,
+        germination: '8 to 10'
+
+
+    }, 
+    {
+        id: 3,
+        name: 'Borlotti bean',
+        image: 'borlb.jfif',
+        description: 'a medium bush variety',
+        price:  3,
+        germination: '5 to 10'
+
+
+    }, 
+    {
+        id: 4,
+        name: 'Christmas Lima bean',
+        image: 'xmaslima.jfif',
+        description: 'a large bush variety',
+        price:  3,
+        germination: '8 to 16'
+
 
     }
 
 
-
-]
+];
